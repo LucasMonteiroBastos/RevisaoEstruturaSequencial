13357452f2fbb75bdd33e0f36c8a0e3530eeff05
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-01-01 21:58:13 -0300

    Feito o ajuste

--- uri1010/Main.java
+++ uri1010/Main.java
@@ -8,24 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int codPeca = sc.nextInt();
 		int peca = sc.nextInt();
 		double valorPeca = sc.nextDouble();
-		
+
 		int codPeca2 = sc.nextInt();
 		int peca2 = sc.nextInt();
-		double valorPeca2= sc.nextDouble();
-		
+		double valorPeca2 = sc.nextDouble();
+
 		double pecaValor1 = peca * valorPeca;
 		double pecaValor2 = peca2 * valorPeca2;
-		
+
 		double valorTotal = pecaValor1 + pecaValor2;
-		
-		System.out.printf("VALOR A PAGAR: R$ %.2f%n",valorTotal);
-		
-		
-		
+
+		System.out.printf("VALOR A PAGAR: R$ %.2f%n", valorTotal);
 
 	}
 
