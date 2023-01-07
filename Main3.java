8ed93a2776c73a317b971919d50e02d05471d1dc
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2023-01-07 01:28:01 -0300

    Funcao em Java

--- Main3.java
+++ Main3.java
@@ -4,22 +4,33 @@ public class Main3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		String s1, s2, s3;
-		int x;
-
-		x = sc.nextInt();
-		sc.nextLine();
-		s1 = sc.nextLine();
-		s2 = sc.nextLine();
-		s3 = sc.nextLine();
-
-		System.out.println("DADOS DIGITADOS: ");
-		System.out.println(x);
-		System.out.println("Valor de s1: " + s1);
-		System.out.println("Valor de s2: " + s2);
-		System.out.println("Valor de s3: " + s3);
-
+		
+		System.out.println("Entre com 3 numeros: ");
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		int higher = max(n1,n2,n3);
+		
+		showResult(higher);
 	}
-
+	
+	public static int max (int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		}
+		else if (y > z) {
+			aux = y;
+		}
+		else {
+			aux = z;
+		}
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("O valor mais alto é: " + value);
+	}
 }
